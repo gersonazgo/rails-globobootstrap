4a26bc9ea1b2048713776a3ed2f839c4db6bfa4b
nsigustavo <nsigustavo@gmail.com>
2012-04-19 20:02:08 -0300

    download builder reading css file accordinly

--- docs/build/download.js
+++ docs/build/download.js
@@ -1,19 +1,19 @@
-var http = require('http');
-var zipper = require('zipper').Zipper;
-var fs = require("fs");
-var zip = require("node-native-zip");
-var less = require("less")
-
+var http = require('http'),
+    zipper = require('zipper').Zipper,
+    fs = require("fs"),
+    zip = require("node-native-zip"),
+    less = require("less"),
+    resolvePath = require('path').resolve;
 
 var  parser = new(less.Parser)({
-        paths: ['./lib'],
+        paths: [resolvePath(__dirname + '/../../lib')],
         filename: 'bootstrap.less'
 })
 
 http.createServer(function (req, res) {
 
     var archive = new zip();
-    var css_less = fs.readFileSync( './lib/bootstrap.less', 'utf-8');
+    var css_less = fs.readFileSync( resolvePath(__dirname + '/../../lib/bootstrap.less'), 'utf-8' );
     parser.parse(css_less, function (e, tree) {
         var css = tree.toCSS({ compress: false })
         archive.add("bootstrap.css", new Buffer(css, "utf8"));
