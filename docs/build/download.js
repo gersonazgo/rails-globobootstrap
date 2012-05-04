852e0775e2f02cbb1fab0b7a68b95569996c547a
Alexandre Magno <alexanmtz@gmail.com>
2012-05-04 17:50:04 -0300

    setting up ip from ubuntu machine to test the post request

--- docs/build/download.js
+++ docs/build/download.js
@@ -45,7 +45,7 @@ http.createServer(function (req, res) {
     });
 
 }).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at http://10.2.121.202:1337/');
 
 
 BootstrapZipBuilder = function(){
