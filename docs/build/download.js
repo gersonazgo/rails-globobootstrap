743110690749f1310e0d9ac29a6a5ec251d2ea25
Guilherme Garnier <guilherme.garnier@gmail.com>
2012-07-19 19:05:32 -0300

    Replacing node-native-zip with node-zip

--- docs/build/download.js
+++ docs/build/download.js
@@ -1,6 +1,6 @@
 var http = require('http'),
     fs = require("fs"),
-    zip = require("node-native-zip"),
+    zip = require("node-zip"),
     less = require("less"),
     util = require("util"),
     qs = require('querystring'),
@@ -11,14 +11,15 @@ http.createServer(function (req, res) {
             'Content-Type': 'application/zip',
             'Cache-Control': 'no-cache',
             'Content-Disposition': 'attachment; filename=bootstrap.zip'
-    });
-   var data = '';
+    })
+   var data = ''
    req.on("data", function(chunk) {
-        data += chunk;
-    });
+        data += chunk
+    })
     req.on("end", function() {
-        var post = qs.parse(data);
-        var archive = new BootstrapZipBuilder();
+        var post = qs.parse(data)
+        var archive = new BootstrapZipBuilder()
+
         for (var variable in post) {
             if (variable[0] == "@"){
                 archive.setVariable(variable, post[variable])
@@ -38,47 +39,49 @@ http.createServer(function (req, res) {
               archive.addJavascript(post.js[i])
           }
         }
-        var buffer = archive.toBuffer()
+        
+        var buffer = archive.generate({base64:true,compression:'STORE'})
+        //fs.writeFileSync('teste.zip', buffer, 'base64')
         res.write(buffer)
         res.end()
-    });
+    })
 
-}).listen(1337, '0.0.0.0');
-console.log('Server running...');
+}).listen(1337, '0.0.0.0')
+console.log('Server running...')
 
 
 BootstrapZipBuilder = function(){
-    this.pathBootstrap = resolvePath(__dirname + '/../../');
-    this.variables = fs.readFileSync( this.pathBootstrap + '/lib/variables.less', 'utf-8' );
-    this.mixins = fs.readFileSync( this.pathBootstrap + '/lib/mixins.less', 'utf-8' );
+    this.pathBootstrap = resolvePath(__dirname + '/../../')
+    this.variables = fs.readFileSync( this.pathBootstrap + '/lib/variables.less', 'utf-8' )
+    this.mixins = fs.readFileSync( this.pathBootstrap + '/lib/mixins.less', 'utf-8' )
 }
-BootstrapZipBuilder.prototype = new zip()
+BootstrapZipBuilder.prototype = new JSZip()
 BootstrapZipBuilder.prototype.addLessCss = function(name){
-    var addCSS = this;
-    var content = this.mixins + this.variables  + this.readCSSLess(name);
-    console.info('antes do parse:' + name)
+    var addCSS = this
+    var content = this.mixins + this.variables  + this.readCSSLess(name)
+
     parser.parse(content, function (e, tree) {
         if (e) { return console.log(e) }
         try {
           var css = tree.toCSS({ compress: false })
-          addCSS.add(name + '.css', new Buffer(css, "utf8")) 
-          console.log('css file: ' + name + '.css' ) 
+
+          addCSS.file(name + '.css', css)
+          console.log('css file: ' + name + '.css')
         } catch(e) {
           console.log(e)
         }
-        
-    });
+    })
 }
 BootstrapZipBuilder.prototype.addJavascript = function(name){
     var content = this.readJavascript(name)
-    this.add( name + '.js', new Buffer(content, "utf8") )
+    this.add( name, new Buffer(content, "utf8") )
 }
 BootstrapZipBuilder.prototype.setVariable = function(name, value){
         this.variables += ( name + ": " + value + ";\n" )
 }
 BootstrapZipBuilder.prototype.readJavascript = function(name){
-    console.log('js file: ' + name);
-    return fs.readFileSync( this.pathBootstrap + '/js/'+ name + '.js', 'utf-8' )
+    console.log('js file: ' + name)
+    return fs.readFileSync( this.pathBootstrap + '/js/'+ name, 'utf-8' )
 }
 BootstrapZipBuilder.prototype.readCSSLess = function(name){
     return fs.readFileSync( this.pathBootstrap + '/lib/'+ name + '.less', 'utf-8' )
