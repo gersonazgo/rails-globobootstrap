272e60983a63f453f64bd147697ddc04739d1125
Guilherme Garnier <guilherme.garnier@gmail.com>
2012-07-20 15:01:22 -0300

    Compressing Javascript files

--- docs/build/download.js
+++ docs/build/download.js
@@ -4,7 +4,9 @@ var http = require('http'),
     less = require("less"),
     util = require("util"),
     qs = require('querystring'),
-    resolvePath = require('path').resolve
+    resolvePath = require('path').resolve,
+    jsp = require("uglify-js").parser,
+    pro = require("uglify-js").uglify
 
 http.createServer(function (req, res) {
     res.writeHead(200, {
@@ -47,8 +49,6 @@ http.createServer(function (req, res) {
         
         var zipfile = archive.generate({base64:true,compression:'STORE'})
         res.write(new Buffer(zipfile, 'base64'))
-        archive.remove('js')
-        archive.remove('css')
         res.end()
     })
 
@@ -96,9 +96,18 @@ BootstrapZipBuilder.prototype.generateSingleCSS = function() {
 BootstrapZipBuilder.prototype.addJavascript = function(name){
     var content = this.readJavascript(name)
     this.file('js/'+ name + '.js', content)
+
+    var compressedContent = this.compressJavascript(content)
+    this.file('js/'+ name + '.min.js', compressedContent)
+}
+BootstrapZipBuilder.prototype.compressJavascript = function(content){
+    var ast = jsp.parse(content)
+    ast = pro.ast_mangle(ast)
+    ast = pro.ast_squeeze(ast)
+    return pro.gen_code(ast)
 }
 BootstrapZipBuilder.prototype.setVariable = function(name, value){
-        this.variables += ( name + ": " + value + ";\n" )
+    this.variables += ( name + ": " + value + ";\n" )
 }
 BootstrapZipBuilder.prototype.readJavascript = function(name){
     console.log('js file: ' + name + '.js')
